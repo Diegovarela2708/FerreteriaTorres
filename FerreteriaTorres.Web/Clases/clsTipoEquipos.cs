94e075afa36edfbd8bc30c121cb4ffb3dba058cc
DIEGO FERNANDO ALVAREZ VARELA <diegoalvarez286818@correo.itm.edu.co>
2021-06-29 22:34:01 -0500

    trabajando en el combo de tipode equipo

---
+++ FerreteriaTorres.Web/Clases/clsTipoEquipos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FerreteriaTorres.Web.Clases
+{
+    public class clsTipoEquipos
+    {
+        #region "Atributos / Propiedades"
+
+        private string strApp;
+
+        public string Error { get; private set; }
+
+        private string strSQL;
+
+        #endregion
+
+        #region "Constructor"
+
+        public clsTipoEquipos(string Aplicacion)
+        {
+            strApp = Aplicacion;
+            strSQL = string.Empty;
+            Error = string.Empty;
+        }
+
+        #endregion
+
+        #region "Metodos Pubicos"
+
+        #endregion
+    }
+}
